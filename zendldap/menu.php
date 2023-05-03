8c05cb690cd85b5aa732aea0baddd471d48b0d0c
joasan22daw <113696261+joasan22daw@users.noreply.github.com>
2023-05-03 19:21:39 +0200

    Add files via upload

---
+++ zendldap/menu.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>
+Menú
+</title>
+</head>
+<body>
+<h2>Menú</h2>
+<a href="visualitzarUsuari.php">Visualitza</a> <br>
+<a href="crearUsuari.php">Crea</a><br>
+<a href="eliminarUsuari.php">Elimina</a><br>
+<a href="modificarUsuari.php">Modifica</a><br>
+<a href="esborraSysdev.php">Esborra sysdev</a><br>
+</body>
+</html>
